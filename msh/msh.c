00b8168eacad9859f7b26505e887bd0f5424713e
JoshuaM4818 <122950704+JoshuaM4818@users.noreply.github.com>
2024-02-13 17:15:40 +0000

    progesss

--- msh/msh.c
+++ msh/msh.c
@@ -1,22 +1,3 @@
-/*
-#define _GNU_SOURCE
-
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h>
-
-
-int main( int argc, char * argv[] )
-{
-  return 0;
-}
-*/
-
-//-----------------MSH EXAMPLE CODE----------------------------------------------
-
 #define _GNU_SOURCE
 
 #include <stdio.h>
@@ -38,17 +19,20 @@ int main( int argc, char * argv[] )
 
 #define MAX_NUM_ARGUMENTS 32     
 
-int main()
+int main( int argc, char * argv[] )
 {
 
   char * command_string = (char*) malloc( MAX_COMMAND_SIZE );
+
+  // error message to print 
+  char error_message[30] = "An error has occurred\n";         
 
   while( 1 )
   {
     // Print out the msh prompt
     printf ("msh> ");
 
-    // Read the command from the command line.  The
+    // Read the command from the commandi line.  The
     // maximum command that will be read is MAX_COMMAND_SIZE
     // This while command will wait here until the user
     // inputs something.
@@ -71,73 +55,92 @@ int main()
     
     char *head_ptr = working_string;
     
-    // Tokenize the input with whitespace used as the delimiter
+    // Tokenize the input with(out) whitespace used as the delimiter
+
     while ( ( (argument_pointer = strsep(&working_string, WHITESPACE ) ) != NULL) &&
               (token_count<MAX_NUM_ARGUMENTS))
     {
-      token[token_count] = strndup( argument_pointer, MAX_COMMAND_SIZE );
-      if( strlen( token[token_count] ) == 0 )
-      {
-        token[token_count] = NULL;
-      }
-        token_count++;
+        // grabs only the non-zero values, tokenizes each command seperately
+        if (strlen(argument_pointer) > 0)                                                 
+        {
+            token[token_count] = strndup( argument_pointer, MAX_COMMAND_SIZE );
+            token_count++;
+        }
     }
+    //token[token_count++] = NULL;
 
-    
+        //-------------------------------------------------------------------------------------------------------------------------
 
+        if (token_count > 0)
+        {
 
-
-
-
-    if (token_count > 0) {
-    // Handle built-in commands
-    if (strcmp(token[0], "exit") == 0) {
-        // Free allocated memory before exiting
-        free(command_string);
-        free(working_string);
-        exit(0);
-    }
-    // Add more built-in commands here (e.g., cd)
-
-    // Fork a child process to execute external commands
-    pid_t pid = fork();
-
-    if (pid < 0) {
-        // Error forking
-        perror("fork");
-    } else if (pid == 0) {
-        // Child process
-        execvp(token[0], token);
-        // execvp only returns if an error occurs
-        perror("execvp");
-        exit(EXIT_FAILURE);
-    } else {
-        // Parent process
-        // Wait for the child to finish
-        waitpid(pid, NULL, 0);
-    }
-}
-
-// Free allocated memory
-free(working_string);
-for (int i = 0; i < token_count; i++) {
-    free(token[i]);
-}
+          if (token[0] && strcmp("exit", token[0]) == 0)        // if exit, close the program
+          {
+            exit(0);
+          }
+ 
+          else if (token[0] && strcmp("cd", token[0]) == 0)
+          {
+            if (token_count == 1)
+            {
+              if (chdir("") == -1) 
+              {
+                // print error if token failed
+                write(STDERR_FILENO, error_message, strlen(error_message)); 
+              }
+            }
+            else if (token_count != 2) 
+            {
+              // print error if theres more than one arg attached to cd
+              write(STDERR_FILENO, error_message, strlen(error_message));             
+            } 
+            else 
+            {
+              if (chdir(token[1]) == -1) 
+              {
+                // print error if second token failed
+                write(STDERR_FILENO, error_message, strlen(error_message)); 
+              }
+            }
+          }
 
 
-    // Now print the tokenized input as a debug check
-    // \TODO Remove this code and replace with your shell functionality
-/*
+
+
+           else if (token[0] && strcmp("ls", token[0]) == 0)
+          {
+            pid_t child_pid1 = fork();
+            int status;
+            if(child_pid1 == -1)
+            {
+              write(STDERR_FILENO, error_message, strlen(error_message)); 
+              exit(-1);
+            }
+            else if (child_pid1 == 0)                 // Child process
+            {
+              execl("/bin/ls", "ls", NULL );
+              exit(0);
+            }
+            else                                      // Parent waiting for child 
+            {
+              waitpid(child_pid1, &status, 0 );
+              fflush( NULL );
+            }
+        }  
+          
+        }
+  // /*
+    // Code to print out each individual token
     int token_index  = 0;
     for( token_index = 0; token_index < token_count; token_index ++ ) 
     {
       printf("token[%d] = %s\n", token_index, token[token_index] );  
     }
 
+//*/
     free( head_ptr );
-*/
+
   }
   return 0;
   // e2520ca2-76f3-90d6-0242ac1210022
 }
-
