639f082e00dac388cac9825c52da4ff64435a5f5
Benjamin Niccum <btn1609@mavs.uta.edu>
2024-02-13 22:01:09 -0600

    Cleaned code

--- msh/msh.c
+++ msh/msh.c
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 {
   char *line = NULL;
   size_t size = 0;
-  ssize_t chars = -1;
+  ssize_t chars;
   char del[2] = " ";
   pid_t pid;
 
@@ -48,153 +48,129 @@ int main(int argc, char *argv[])
   {
     printf("msh> ");
     chars = getline(&line, &size, stdin); // returned value is length of string + 1 for line break
-    while(chars > 0 && isspace(line[chars - 1])) //Removes extraneous spaces at end of last command
+    if (chars <= 0)
     {
-      line[chars - 1] = '\0';
-      chars--;
+      printf("An error has occurred");
     }
-    line[chars] = '\0'; //Ensures last character is a null terminator
-
-    int argc1 = 1; //argc will always be at least 1
-    for(int i = 0;i<chars -1;i++) //counts argc
+    else 
     {
-      if(line[i] == ' ') //space indicates a new command
+      while(chars > 0 && isspace(line[chars - 1])) //Removes extraneous spaces at end of last command
       {
-        if(line[i-1] == ' ') //two spaces in a row is a bad command
+        line[chars - 1] = '\0';
+        chars--;
+      }
+      line[chars] = '\0'; //Ensures last character is a null terminator
+      int argc1 = 1; //argc will always be at least 1
+      for(int i = 0;i<chars -1;i++) //counts argc
+      {
+        if(line[i] == ' ') //space indicates a new command
         {
-          printf("Please enter only one space between commands\n");
-          goto Start;     //ignores bad command and returns to Start:
+          if(line[i-1] == ' ') //two spaces in a row is a bad command
+          {
+            printf("An error has occured\n");
+            goto Start;     //ignores bad command and returns to Start:
+          }
+          argc1++;
         }
-        argc1++;
       }
-    }
-    //printf("argc = %d\n", argc1); //For testing
-    
-    
 
-    char argv1[argc1][40];
-    char temp[256];
-    strcpy(temp, line); // keeps original copy safe from strtok
-    strcpy(argv1[0],strtok(temp, del)); //assigns first command
-    if(argc1 > 1)
-    {
-      
-      for(int i = 1;i < argc1;i++)     //assigns each additional command
+      char argv1[argc1][40];
+      char temp[256];
+      strcpy(temp, line); // keeps original copy safe from strtok
+      strcpy(argv1[0],strtok(temp, del)); //assigns first command
+      if(argc1 > 1)
       {
-        strcpy(argv1[i],strtok(NULL, del));
+        for(int i = 1;i < argc1;i++)     //assigns each additional command
+        {
+          strcpy(argv1[i],strtok(NULL, del));
+        }
       }
-      for(int i = 0;i < argc1;i++) //prints all argv1 entries seperately
-      {
-        //printf("{%s}\n", argv1[i]);
-      }
-    }
-    //At this point, Custom Argc and Argv are functional
+      //At this point, Custom Argc and Argv are functional
 
-    if (chars < 0) //Something went terribly wrong here.
-    {
-      puts("An error has occured"); 
-    }
-    else if(strcmp(line, "exit") == 0) //user input exit command
-    {
-      exit(0);
-    }
-    else if(strcmp(argv1[0], "cd") == 0) //user is changing directory
-    {
-      //printf("Moving to new directory %s\n", argv1[1]);
-      chdir(argv1[1]);
-    }
-    else //User entered an external command
-    {
-      
-        //This section taken from popen.c in examples
-      pid = fork();
-      if(pid == 0)
+      if (chars < 0) //Something went terribly wrong here.
       {
+        puts("An error has occured"); 
+      }
+      else if(strcmp(line, "exit") == 0) //user input exit command
+      {
+        exit(0);
+      }
+      else if(strcmp(argv1[0], "cd") == 0) //user is changing directory
+      {
+        chdir(argv1[1]);
+      }
+      else //User entered an external command
+      {
+        pid = fork();
+        if(pid == 0)
+        {
           //child
-          //execl("/bin/ls", "ls", NULL );
-          //exit( EXIT_SUCCESS );
-          char cat[40];
-          sprintf(cat, "%s/%s", "/bin/", argv1[0]);
-          for(int i = 5;i< strlen(argv1[0]) + 5;i++)
-          {
-            cat[i] = argv1[0][i-5];
-          }
-          cat[strlen(cat) - 1]= '\0';
-          //printf("{%s}\n", cat);
-          
-          if(access(cat, X_OK) == 0) //checks if File is valid. 
-          {
-            switch (argc1)
-            {
-            case (1):
-            execl(cat, argv1[0], NULL);
-            break;
-            case (2): 
-            execl(cat, cat, argv1[1], NULL);
-            break;
-            case (3):
-            execl(cat, cat, argv1[1], argv1[2], NULL);
-            break;
-            case (4):
-            execl(cat, cat, argv1[1], argv1[2], argv1[3], NULL);
-            break;
-            case (5):
-            execl(cat, cat, argv1[1], argv1[2], argv1[3], argv1[4], NULL);
-            break;
-            case (6):
-            execl(cat, cat, argv1[1], argv1[2], argv1[3], argv1[4], argv1[5], NULL);
-            break;
-            default:
-            printf("Too many arguments");
-            break;
-            }
-          }
-          else
-          {
-            printf("Command not found: %s\n", argv1[0]);
-          }
-
-          //exit( EXIT_SUCCESS );
-        
+          //////////////////////////////////////redirect
           for(int i = 1;i < argc1;i++)
           {
             if(strcmp(argv1[i], ">") == 0)  //allows > as a command to pipe result into file
             {
-            //open file called "entered command". if it doesn't exist, create it.
-            int fd = open(argv[i + 1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR); 
-            //O_RDWR | O_CREAT, S_IRUSR | S_IWUSR  //From popen.c
-            if(fd < 0)
-            {
-              perror("Can't open output file.");  //prints error if file can not be created or found
-              exit(0);                    
+              //open file called "entered command". if it doesn't exist, create it.
+              int fd = open(argv[i + 1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR); 
+              if(fd < 0)
+              {
+                perror("Can't open output file.");  //prints error if file can not be created or found
+                exit(0);                    
+              }
+              dup2(fd, 1);
+              close(fd);
+              printf("File Updated\n");
             }
-            dup2(fd, 1);
-            close(fd);
-            printf("File Updated\n");
-            }
-          
-         }
+          }
+          /////////////////////////////////////////
 
-         return 0;
+          char cat[40];
+          sprintf(cat, "%s/%s", "/bin/", argv1[0]);
+    
+          if(access(cat, X_OK) == 0) //checks if command exists in Bin. 
+          {
+            switch (argc1)  //This switch is based on how many arguments are enterred into the msh shell.
+            {
+              case (1):
+              execl(cat, argv1[0], NULL);
+              break;
+              case (2): 
+              execl(cat, cat, argv1[1], NULL); //found mulitple argument method on BlackBerry QNX website
+              break;
+              case (3):
+              execl(cat, cat, argv1[1], argv1[2], NULL);
+              break;
+              case (4):
+              execl(cat, cat, argv1[1], argv1[2], argv1[3], NULL);
+              break;
+              case (5):
+              execl(cat, cat, argv1[1], argv1[2], argv1[3], argv1[4], NULL);
+              break;
+              case (6):
+              execl(cat, cat, argv1[1], argv1[2], argv1[3], argv1[4], argv1[5], NULL);
+              break;
+              default:
+              printf("An error has occured");
+              break;
+            }
+          }
+          else
+          {
+            printf("An error has occured");
+            return 0;
+          }
+        }
+        else if(pid > 0)
+        {
+          //parent
+          wait(NULL);
+        }
+        else
+        {
+          perror("Fork failed.");
+        }
       }
-      else if(pid > 0)
-      {
-        //parent
-        wait(NULL);
-        //printf("Parent is awake\n");
-      }
-      else
-      {
-        perror( "Fork failed." );
-      }
-      
-      //printf("%s is not recognized in MSH\n", line);
     }
   }
-  free(line);
   return 0;
 }
-//fork()
-//implement exec() execvp() or execv()
-// /bin or /users/bin
-//Ctrl d to exit bash
