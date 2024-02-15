76ca5c2e9b22f064f0fa7e2696462a3a949a3bdd
AbiPMZ <111903767+AbiPMZ@users.noreply.github.com>
2024-02-15 03:41:04 +0000

    start

--- msh/msh.c
+++ msh/msh.c
@@ -29,10 +29,85 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <fcntl.h>
 
+// void process_command(char* command_str);
 
 int main( int argc, char * argv[] )
 {
+
+  //char *command_str = ()
+
+
+// if ( == "exit") {}
+
+
+
+   pid_t pid = fork( );
+
+   if( pid == 0 )
+   {
+      // Iterate over the provided command and see if there is a redirect
+      // operator in it.  If there is then open a pipe between 
+      int i;
+      for( i=1; i<argc; i++ )
+      {
+         if( token[0] && strcmp( argv[i], ">" ) == 0 )
+         {
+            int fd = open( argv[i+1], O_RDWR | O_CREAT, S_IRUSR | S_IWUSR );
+            if( fd < 0 )
+            {
+                perror( "Can't open output file." );
+                exit( 0 );                    
+            }
+            dup2( fd, 1 );
+            close( fd );
+            
+            // Trim off the > output part of the command
+            argv[i] = NULL;
+         }
+      }
+      execvp( argv[1], &argv[1] );
+   }
+  else if( pid > 0 )
+  {
+    wait( NULL );
+  }
+  else
+  {
+    perror( "Fork failed." );
+  }
+
+//REDIRECTION OUTPUT - forks then string compares. parent just waits
+  // pid_t pid = fork(); 
+
+  // if (pid == 0){
+
+  //   if (token[0] && strcmp (token[0], ">") == 0) {
+  //     printf("Found one\n");
+  //   else {
+  //     printf("Didn't find one\n");
+  //   }
+
+  //   printf("Done searching\n");
+    
+  //   }
+
+  //   else 
+  //   {
+  //     int status;
+  //     wait (&status);
+
+
+  //   }
+
+  // }
+
+
+
   return 0;
 }
 
