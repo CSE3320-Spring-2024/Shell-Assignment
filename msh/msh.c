855ba10b82675450a16a4228c069a5a926bcbf07
Andy <thanhan22595@gmail.com>
2024-02-19 03:14:20 +0000

    stuck at test3

--- msh/msh.c
+++ msh/msh.c
@@ -1,18 +1,18 @@
 // The MIT License (MIT)
-// 
-// Copyright (c) 2024 Trevor Bakker 
-// 
+//
+// Copyright (c) 2024 Trevor Bakker
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
-// 
+//
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,14 +25,147 @@
 
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
+#define WHITESPACE " \t\n"
+#define MAX_COMMAND_SIZE 255
+#define MAX_NUM_ARGUMENTS 32
 
-int main( int argc, char * argv[] )
+int main(int argc, char *argv[])
 {
+  char error_message[30] = "An error has occurred\n";
+
+  // Check if the shell is invoked with correct arguments
+  if (argc > 2)
+  {
+    // Print error message and exit with status 1
+    write(STDERR_FILENO, error_message, strlen(error_message));
+    exit(1);
+  }
+
+  char *command_string = (char *)malloc(MAX_COMMAND_SIZE);
+
+  // Determine input source: batch mode or interactive mode
+  FILE *input_file = NULL;
+  if (argc == 2)
+  {
+    // Batch mode
+    input_file = fopen(argv[1], "r");
+    if (input_file == NULL)
+    {
+      // Print error message and exit with status 1
+      write(STDERR_FILENO, error_message, strlen(error_message));
+      exit(1);
+    }
+  }
+
+  // Loop for interactive or batch mode
+  while (1)
+  {
+    if (input_file == NULL)
+    {
+      // Interactive mode: print prompt
+      printf("msh> ");
+      // Read command from stdin
+      if (!fgets(command_string, MAX_COMMAND_SIZE, stdin))
+      {
+        // End of file (Ctrl+D), exit gracefully
+        break;
+      }
+    }
+    else
+    {
+      // Batch mode: read command from file
+      if (!fgets(command_string, MAX_COMMAND_SIZE, input_file))
+      {
+        // End of file, exit gracefully
+        break;
+      }
+    }
+
+    // Tokenize input command
+    char *token[MAX_NUM_ARGUMENTS];
+    int token_count = 0;
+    char *argument_pointer;
+    char *working_string = strdup(command_string);
+    char *head_ptr = working_string;
+
+    while (((argument_pointer = strsep(&working_string, WHITESPACE)) != NULL) &&
+           (token_count < MAX_NUM_ARGUMENTS))
+    {
+      token[token_count] = strndup(argument_pointer, MAX_COMMAND_SIZE);
+      if (strlen(token[token_count]) == 0)
+      {
+        token[token_count] = NULL;
+      }
+      token_count++;
+    }
+
+    // Execute built-in commands: exit and cd
+    if (strcmp(token[0], "exit") == 0)
+    {
+      // Exit the shell
+      exit(0);
+    }
+    else if (strcmp(token[0], "cd") == 0)
+    {
+      // Change directory
+      if ((token_count - 1) != 2)
+      {
+        // Print error message for invalid arguments
+        write(STDERR_FILENO, error_message, strlen(error_message));
+      }
+      else
+      {
+        // Change directory
+        if (chdir(token[1]) == -1)
+        {
+          // Print error message for failed directory change
+          write(STDERR_FILENO, error_message, strlen(error_message));
+        }
+      }
+    }
+    else
+    {
+      // Execute external commands
+      pid_t pid = fork();
+      if (pid == -1)
+      {
+        // Fork failed, print error message
+        write(STDERR_FILENO, error_message, strlen(error_message));
+      }
+      else if (pid == 0)
+      {
+        // Child process: execute command
+        execv(token[0], token);
+        // If execv returns, an error occurred
+        write(STDERR_FILENO, error_message, strlen(error_message));
+        exit(EXIT_FAILURE);
+      }
+      else
+      {
+        // Parent process: wait for child to terminate
+        int status;
+        waitpid(pid, &status, 0);
+      }
+    }
+
+    // Free allocated memory
+    free(head_ptr);
+  }
+
+  // Close input file if opened
+  if (input_file != NULL)
+  {
+    fclose(input_file);
+  }
+
+  // Free allocated memory
+  free(command_string);
+
   return 0;
 }
-
