5907e3452204d3d98870c21078e290cbd6f5d1bf
Prakhyat Chaube <pxc0272@mavs.uta.edu>
2024-02-20 15:50:33 -0600

    First Push

--- msh/msh.c
+++ msh/msh.c
@@ -1,40 +1,168 @@
-/*Prakhyat Chaube 
-1002060272*/
-// The MIT License (MIT)
-// 
-// Copyright (c) 2024 Trevor Bakker 
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 #define _GNU_SOURCE
-
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdarg.h>
 
+#define WHITESPACE " \t\n"
+#define SIZE_OF_COMMAND_MAX 255
+#define NUM_OF_ARGUMENTS_MAX 10
+#define HIST_OF_COMMAND_MAX 15
+#define PID_OF_COMMAND_MAX 15
 
-int main( int argc, char * argv[] )
+char historyOfCmds[HIST_OF_COMMAND_MAX][SIZE_OF_COMMAND_MAX];
+int numOfPidsCmds[PID_OF_COMMAND_MAX];
+int toTrackTheCmds= 0;
+int toTrackThePids = 0;
+bool checkTheInput = true;
+
+void toPrintTheHistory(int trackNum)
 {
-  return 0;
+  for (int i = 0; i < trackNum; i++)
+  {
+    printf("%d: %s", i + 1, historyOfCmds[i]);
+  }
 }
 
+void toPrintThePids(int trackNum)
+{
+  for (int i = 0; i < trackNum; i++)
+  {
+    printf("%d: %d\n", i + 1, numOfPidsCmds[i]);
+  }
+}
+
+void toPrintTheError()
+{
+    const char *error_message = "An error has occurred\n";
+    write(STDERR_FILENO, error_message, strlen(error_message));
+}
+
+void toHandleTheBuiltIns(char *token[])
+{
+  if (strcmp("exit", token[0]) == 0)
+  {
+    exit(0);
+  }
+  else if (strcmp("cd", token[0]) == 0)
+  {
+    if (token[1] == NULL || token[2] != NULL)
+    {
+      toPrintTheError();
+    }
+    else
+    {
+      if (chdir(token[1]) == -1)
+      {
+        perror("cd");
+      }
+    }
+  }
+  else if (strcmp("cd", token[0]) == 0)
+  {
+    if (token[1] == NULL)
+    {
+      const char *home_dir = getenv("HOME");
+      if (home_dir != NULL)
+      {
+        if (chdir(home_dir) == -1)
+        {
+          perror("cd");
+        }
+      }
+      else
+      {
+        toPrintTheError();
+      }
+    }
+    else if (token[2] != NULL)
+    {
+      toPrintTheError();
+    }
+    else
+    {
+      if (chdir(token[1]) == -1)
+      {
+        perror("cd");
+      }
+    }
+  }
+  else
+  {
+    pid_t pid = fork();
+    if (pid == -1)
+    {
+      perror("An error has occurred\n");
+      exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+      execvp(token[0], token);
+      toPrintTheError();
+      exit(EXIT_FAILURE);
+    }
+    else
+    {
+      int status;
+      waitpid(pid, &status, 0);
+    }
+  }
+}
+
+int main()
+{
+  char *allocate_cmdStr = (char *)malloc(SIZE_OF_COMMAND_MAX);
+  while (1)
+  {
+    if (checkTheInput)
+    {
+      printf("msh> ");
+      while (!fgets(allocate_cmdStr, SIZE_OF_COMMAND_MAX, stdin));
+    }
+
+    char *tokenWeHave[NUM_OF_ARGUMENTS_MAX];
+    int numOfTokens = 0;
+    char *pointerForArgument;
+    char *stringWorks = strdup(allocate_cmdStr);
+    char *rootWorks = stringWorks;
+    checkTheInput = true;
+    while (((pointerForArgument = strsep(&stringWorks, WHITESPACE)) != NULL) &&
+           (numOfTokens < NUM_OF_ARGUMENTS_MAX))
+    {
+      tokenWeHave[numOfTokens] = strndup(pointerForArgument, SIZE_OF_COMMAND_MAX);
+      if (strlen(tokenWeHave[numOfTokens]) == 0)
+      {
+        tokenWeHave[numOfTokens] = NULL;
+      }
+      numOfTokens++;
+    }
+    if (tokenWeHave[0] != NULL && allocate_cmdStr[0] != '!')
+    {
+      if (toTrackTheCmds< HIST_OF_COMMAND_MAX)
+      {
+        strcpy(historyOfCmds[toTrackTheCmds], allocate_cmdStr);
+        toTrackTheCmds++;
+      }
+      else
+      {
+        for (int row = 0; row < HIST_OF_COMMAND_MAX - 1; row++)
+        {
+          strcpy(historyOfCmds[row], historyOfCmds[row + 1]);
+        }
+        strcpy(historyOfCmds[HIST_OF_COMMAND_MAX - 1], allocate_cmdStr);
+      }
+    }
+    if (tokenWeHave[0] == NULL)
+    {
+      continue;
+    }
+    toHandleTheBuiltIns(tokenWeHave);
+    free(rootWorks);
+  }
+  return 0;
+}
