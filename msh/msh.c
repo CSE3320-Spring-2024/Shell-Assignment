36485953a55edcaf576add95028bcb61265ef388
JXC3149 <107070493+JXC3149@users.noreply.github.com>
2024-02-21 17:49:32 +0000

    Fork, exec, wait, cd, ls, and exit w/ error message

--- msh/msh.c
+++ msh/msh.c
@@ -29,74 +29,109 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <stdbool.h>
 
-#define WHITESPACE " \t\n"      // We want to split our command line up into tokens
-                                // so we need to define what delimits our tokens.
-                                // In this case  white space
-                                // will separate the tokens on our command line
+#define WHITESPACE " \t\n"
+#define MAX_COMMAND_SIZE 255
+#define MAX_NUM_ARGUMENTS 32
 
-#define MAX_COMMAND_SIZE 255    // The maximum command-line size
-
-#define MAX_NUM_ARGUMENTS 32  
-
-int main( int argc, char * argv[] )
+int main()
 {
-  char * command_string = (char*) malloc( MAX_COMMAND_SIZE );
+  char* command_string = (char*) malloc(MAX_COMMAND_SIZE);
+  char* path[] = {"/bin", "/usr/bin", "/usr/local/bin", "./", NULL};
+  
+  while(1) {
+    printf("msh> ");
 
-  while( 1 )
-  {
-    // Print out the msh prompt
-    printf ("msh> ");
+    while(!fgets(command_string, MAX_COMMAND_SIZE, stdin));
 
-    // Read the command from the commandi line.  The
-    // maximum command that will be read is MAX_COMMAND_SIZE
-    // This while command will wait here until the user
-    // inputs something.
-    while( !fgets (command_string, MAX_COMMAND_SIZE, stdin) );
-
-    /* Parse input */
-    char *token[MAX_NUM_ARGUMENTS];
-
+    /* Parse */
+    char* token[MAX_NUM_ARGUMENTS];
+    char* argument_pointer;                                         
+    char* working_string  = strdup(command_string);                
+    char* head_ptr = working_string;
+    char error_message[30] = "An error has occurred\n";
     int token_count = 0;                                 
-                                                           
-    // Pointer to point to the token
-    // parsed by strsep
-    char *argument_pointer;                                         
-                                                           
-    char *working_string  = strdup( command_string );                
-
-    // we are going to move the working_string pointer so
-    // keep track of its original value so we can deallocate
-    // the correct amount at the end
-    
-    char *head_ptr = working_string;
     
     // Tokenize the input with whitespace used as the delimiter
-    while ( ( (argument_pointer = strsep(&working_string, WHITESPACE ) ) != NULL) &&
-              (token_count<MAX_NUM_ARGUMENTS))
-    {
-      token[token_count] = strndup( argument_pointer, MAX_COMMAND_SIZE );
-      if( strlen( token[token_count] ) == 0 )
-      {
+    while(((argument_pointer = strsep(&working_string, WHITESPACE)) != NULL) &&
+         (token_count<MAX_NUM_ARGUMENTS)) {
+      token[token_count] = strndup(argument_pointer, MAX_COMMAND_SIZE);
+      if(strlen(token[token_count]) == 0) {
         token[token_count] = NULL;
       }
-        token_count++;
+
+      token_count++;
     }
 
-    // Now print the tokenized input as a debug check
-    // \TODO Remove this code and replace with your shell functionality
-
+    // Now print the tokenized input as a debug check --REMOVE WHEN SUBMITTING--
     int token_index  = 0;
-    for( token_index = 0; token_index < token_count; token_index ++ ) 
+    for(token_index = 0; token_index < token_count; token_index++) 
     {
-      printf("token[%d] = %s\n", token_index, token[token_index] );  
+      printf("token[%d] = %s\n", token_index, token[token_index]);  
+    }
+
+    // Check for exit
+    if(strcmp(token[0], "exit") == 0) {
+      if(token[1] != NULL) {
+        write(STDERR_FILENO, error_message, strlen(error_message)); 
+        continue;
+      }
+      exit(0);
+    }
+
+    // Check for cds
+    if(strcmp(token[0], "cd") == 0) {
+      if(token[1] == NULL) {
+        write(STDERR_FILENO, error_message, strlen(error_message)); 
+        continue;
+      } else if(token[2] != NULL) {
+        write(STDERR_FILENO, error_message, strlen(error_message)); 
+        continue;
+      }
+      if(chdir(token[1]) != 0) {
+        write(STDERR_FILENO, error_message, strlen(error_message)); 
+      }
+
+      continue;
+    }
+
+    // Check if command exists
+    bool command_exists = false;
+    char command_path[MAX_COMMAND_SIZE];
+
+    for(int i = 0; path[i] != NULL; i++) {
+      snprintf(command_path, MAX_COMMAND_SIZE, "%s/%s", path[i], token[0]);
+      if(access(command_path, X_OK) == 0) {
+        command_exists = true;
+        break;
+      }
+    }
+    if(!command_exists) {
+      write(STDERR_FILENO, error_message, strlen(error_message)); 
+      continue;
+    }
+
+    // The forkening...
+    pid_t pid = fork();
+
+    if(pid < 0) {
+      write(STDERR_FILENO, error_message, strlen(error_message)); 
+      exit(1);
+    } else if(pid == 0) {
+      if (execvp(token[0], token) < 0) {
+        write(STDERR_FILENO, error_message, strlen(error_message)); 
+        exit(1);
+      }
+    } else {
+      wait(NULL);
     }
 
     free( head_ptr );
-
   }
 
-
   return 0;
+  
+  // e2520ca2-76f3-90d6-0242ac1210022
 }
 
