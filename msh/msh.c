f109f1b424792ce49c4ceaa3cb1555b8f4e053d5
PrakhyatC <83581014+prakhyatc@users.noreply.github.com>
2024-02-21 02:23:23 +0000

    Fourth Commit

--- msh/msh.c
+++ msh/msh.c
@@ -1,3 +1,161 @@
+/*#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define WHITESPACE " \t\n"
+#define SIZE_OF_COMMAND_MAX 1024
+#define NUM_OF_ARGUMENTS_MAX 10
+
+
+bool checkTheInput = true;
+
+
+void toPrintTheError()
+{
+    char error_message[] = "An error has occurred\n";
+    write(STDERR_FILENO, error_message, strlen(error_message));
+}
+
+void toHandleTheBuiltIns(char *token[])
+{
+    if (strcmp("exit", token[0]) == 0)
+    {
+        exit(0);
+    }
+    else if (strcmp("cd", token[0]) == 0)
+    {
+        if (token[1] == NULL)
+        {
+            const char *home_dir = getenv("HOME");
+            if (home_dir != NULL)
+            {
+                if (chdir(home_dir) == -1)
+                {
+                    perror("cd");
+                }
+            }
+            else
+            {
+                toPrintTheError();
+            }
+        }
+        else if (token[2] != NULL)
+        {
+            toPrintTheError();
+        }
+        else
+        {
+            if (chdir(token[1]) == -1)
+            {
+                perror("cd");
+            }
+        }
+    }
+    else
+    {
+        pid_t pid = fork();
+        if (pid == -1)
+        {
+            perror("An error has occurred\n");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid == 0)
+        {
+            execvp(token[0], token);
+            toPrintTheError();
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            int status;
+            waitpid(pid, &status, 0);
+        }
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    char *bufferInputWeHave = NULL;
+    size_t bufferInputSizeWeHave = 0;
+    ssize_t bufferInputLengthWeHave;
+    FILE *batchFile;
+
+    if (argc == 2)
+    {
+        batchFile = fopen(argv[1], "r");
+    }
+
+    while (1)
+    {
+        if (argc != 2)
+        {
+            printf("msh> ");
+            fflush(stdout);
+            bufferInputLengthWeHave = getline(&bufferInputWeHave, &bufferInputSizeWeHave, stdin);
+        }
+        else
+        {
+            bufferInputLengthWeHave = getline(&bufferInputWeHave, &bufferInputSizeWeHave, batchFile);
+            if(feof(batchFile)) exit(0);
+        }
+        if(bufferInputLengthWeHave == -1)
+        {
+            if(feof(stdin))
+            {
+                printf("\n");
+                break;
+            }
+            else
+            {
+                toPrintTheError();
+                continue;
+            }
+        }
+
+        if (bufferInputWeHave[bufferInputLengthWeHave - 1] == '\n')
+            bufferInputWeHave[bufferInputLengthWeHave - 1] = '\0';
+
+        if (strcmp(bufferInputWeHave, "exit") == 0) {
+            break;
+        }
+
+        char *tokenWeHave[NUM_OF_ARGUMENTS_MAX];
+        int numOfTokens = 0;
+        char *pointerForArgument;
+        char *stringWorks = strdup(bufferInputWeHave);
+        char *rootWorks = stringWorks;
+        checkTheInput = true;
+
+        while (((pointerForArgument = strsep(&rootWorks, WHITESPACE)) != NULL) && (numOfTokens < NUM_OF_ARGUMENTS_MAX))
+        {
+            tokenWeHave[numOfTokens] = strndup(pointerForArgument, SIZE_OF_COMMAND_MAX);
+            if (strlen(tokenWeHave[numOfTokens]) == 0)
+            {
+                tokenWeHave[numOfTokens] = NULL;
+            }
+            numOfTokens++;
+        }
+
+        if (tokenWeHave[0] != NULL && bufferInputWeHave[0] != '!')
+    {
+
+        if (tokenWeHave[0] == NULL)
+        {
+            continue;
+        }
+        toHandleTheBuiltIns(tokenWeHave);
+        free(rootWorks);
+    }
+
+    return 0;
+}
+}*/
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
@@ -9,30 +167,16 @@
 #define WHITESPACE " \t\n"
 #define SIZE_OF_COMMAND_MAX 255
 #define NUM_OF_ARGUMENTS_MAX 10
-#define HIST_OF_COMMAND_MAX 15
 #define PID_OF_COMMAND_MAX 15
 
-char historyOfCmds[HIST_OF_COMMAND_MAX][SIZE_OF_COMMAND_MAX];
-int numOfPidsCmds[PID_OF_COMMAND_MAX];
+
+
 int toTrackTheCmds = 0;
 int toTrackThePids = 0;
 bool checkTheInput = true;
 
-void toPrintTheHistory(int trackNum)
-{
-    for (int i = 0; i < trackNum; i++)
-    {
-        printf("%d: %s", i + 1, historyOfCmds[i]);
-    }
-}
 
-void toPrintThePids(int trackNum)
-{
-    for (int i = 0; i < trackNum; i++)
-    {
-        printf("%d: %d\n", i + 1, numOfPidsCmds[i]);
-    }
-}
+
 
 void toPrintTheError()
 {
@@ -92,19 +236,6 @@ void toHandleTheBuiltIns(char *token[])
         }
         else
         {
-            if (toTrackThePids < PID_OF_COMMAND_MAX)
-            {
-                numOfPidsCmds[toTrackThePids] = pid;
-                toTrackThePids++;
-            }
-            else
-            {
-                for (int row = 0; row < PID_OF_COMMAND_MAX - 1; row++)
-                {
-                    numOfPidsCmds[row] = numOfPidsCmds[row + 1];
-                }
-                numOfPidsCmds[PID_OF_COMMAND_MAX - 1] = pid;
-            }
 
             int status;
             waitpid(pid, &status, 0);
@@ -136,16 +267,16 @@ int main(int argc, char *argv[])
 
     while (1)
     {
-        if (checkTheInput)
+        if (argc != 2)
         {
             printf("msh> ");
             while (!fgets(allocate_cmdStr, SIZE_OF_COMMAND_MAX, stdin));
         }
-        else if (fgets(allocate_cmdStr, SIZE_OF_COMMAND_MAX, batchFile) == NULL)
+        else 
         {
-            // End of batch file
-            fclose(batchFile);
-            exit(0);
+            while(!fgets(allocate_cmdStr, SIZE_OF_COMMAND_MAX, batchFile)) {
+                if(feof(batchFile)) exit(0);
+            }
         }
 
         char *tokenWeHave[NUM_OF_ARGUMENTS_MAX];
@@ -164,23 +295,6 @@ int main(int argc, char *argv[])
                 tokenWeHave[numOfTokens] = NULL;
             }
             numOfTokens++;
-        }
-
-        if (tokenWeHave[0] != NULL && allocate_cmdStr[0] != '!')
-        {
-            if (toTrackTheCmds < HIST_OF_COMMAND_MAX)
-            {
-                strcpy(historyOfCmds[toTrackTheCmds], allocate_cmdStr);
-                toTrackTheCmds++;
-            }
-            else
-            {
-                for (int row = 0; row < HIST_OF_COMMAND_MAX - 1; row++)
-                {
-                    strcpy(historyOfCmds[row], historyOfCmds[row + 1]);
-                }
-                strcpy(historyOfCmds[HIST_OF_COMMAND_MAX - 1], allocate_cmdStr);
-            }
         }
         if (tokenWeHave[0] == NULL)
         {
